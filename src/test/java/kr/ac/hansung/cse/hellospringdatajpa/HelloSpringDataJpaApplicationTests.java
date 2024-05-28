2403b647bf86c112776d0aa2de678c96766194a1
seungsang <rovin000@hansung.ac.kr>
2024-05-28 23:42:32 +0900

    연습

---
+++ src/test/java/kr/ac/hansung/cse/hellospringdatajpa/HelloSpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.hansung.cse.hellospringdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
