0cfeb355ef062deff72a2b0025aa899881badd9a
seungsang <rovin000@hansung.ac.kr>
2024-05-29 01:06:55 +0900

    연습

--- src/main/java/kr/ac/hansung/cse/hellospringdatajpa/repository/ProductRepository.java
+++ src/main/java/kr/ac/hansung/cse/hellospringdatajpa/repository/ProductRepository.java
@@ -1,7 +1,13 @@
 package kr.ac.hansung.cse.hellospringdatajpa.repository;
 
 import kr.ac.hansung.cse.hellospringdatajpa.entity.Product;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    Product findByName(String name);
+    List<Product> findByNameContaining(String keyword, Pageable paging);
 }
